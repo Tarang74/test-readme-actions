f8c2c2d0307ea8bbad7fe14f5f45c1085c5c3bcb
Tarang74 <tjanawalkar74@gmail.com>
2022-02-26 09:40:59 +1000

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+
+async function run() {
+    try {
+        // Get client and context
+        const client = github.getOctokit(
+            core.getInput('repo-token', { required: true })
+        );
+        const context = github.context;
+
+        console.log('Client: ', client);
+        console.log('Context: ', context);
+
+        return;
+
+    } catch (error: any) {
+        core.setFailed(error.message);
+        return;
+    }
+}
+
+run();
