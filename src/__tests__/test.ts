635d10bee70ab560a61a53267817ec888ddddb23
Tarang74 <tjanawalkar74@gmail.com>
2022-02-28 23:34:00 +1000

    Try using ncc to bundle packages

--- src/__tests__/test.ts
+++ src/__tests__/test.ts
@@ -4,7 +4,7 @@ import fs from 'fs';
 import path from 'path';
 
 test('Example test.', () => {
-    const file = path.join(__dirname, "../", "test-readme-actions Lecture Notes.tex");
+    const file = path.join(__dirname, "../../", "test-readme-actions Lecture Notes.tex");
     const s = fs.readFileSync(file, "utf8");
     
     parseLectureNotesContents(s);
